5df53f097da3f2ec65712611406cb3904e660a6a
abhidigha <abhishek.digha1990@gmail.com>
2023-09-11 11:45:59 +0530

    local driver enabled

--- src/main/java/com/base/Base.java
+++ src/main/java/com/base/Base.java
@@ -21,52 +21,52 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Base {
 
-	//public WebDriver driver;
+	public WebDriver driver;
 	public Properties prop;
-	String username = "ak47myself";
-    String accesskey = "7gkFIO9oOFVQfyE1VITQEzpEGD899Sb1K4hHvDGOe0wsfKSKCG";
-    static RemoteWebDriver driver = null;
-    String gridURL = "@hub.lambdatest.com/wd/hub";
+	//String username = "ak47myself";
+    //String accesskey = "7gkFIO9oOFVQfyE1VITQEzpEGD899Sb1K4hHvDGOe0wsfKSKCG";
+    //static RemoteWebDriver driver = null;
+    //String gridURL = "@hub.lambdatest.com/wd/hub";
     boolean status = false;
 	public WebDriver initializeDriver() throws IOException {
 
 		
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("browserName", "Chrome");
-		capabilities.setCapability("browserVersion", "114.0");
-		HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-		ltOptions.put("platformName", "Windows 10");
-		ltOptions.put("project", "Selenium With Java");
-		ltOptions.put("selenium_version", "3.141.59");
-		capabilities.setCapability("LT:Options", ltOptions);
-		capabilities.setCapability("build", "Test Login -3");
-        capabilities.setCapability("name", "LambdaTest Execution -3");
-		 try {
-	            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
-	        } catch (MalformedURLException e) {
-	            System.out.println("Invalid grid URL");
-	        } catch (Exception e) {
-	            System.out.println(e.getMessage());
-	        }
+//		DesiredCapabilities capabilities = new DesiredCapabilities();
+//		capabilities.setCapability("browserName", "Chrome");
+//		capabilities.setCapability("browserVersion", "114.0");
+//		HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+//		ltOptions.put("platformName", "Windows 10");
+//		ltOptions.put("project", "Selenium With Java");
+//		ltOptions.put("selenium_version", "3.141.59");
+//		capabilities.setCapability("LT:Options", ltOptions);
+//		capabilities.setCapability("build", "Test Login -3");
+//        capabilities.setCapability("name", "LambdaTest Execution -3");
+//		 try {
+//	            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accesskey + gridURL), capabilities);
+//	        } catch (MalformedURLException e) {
+//	            System.out.println("Invalid grid URL");
+//	        } catch (Exception e) {
+//	            System.out.println(e.getMessage());
+//	        }
 		
 		prop = loadConfig();
-//		String browserName = prop.getProperty("browser");
-//
-//		if (browserName.equals("chrome")) {
-//			System.setProperty("webdriver.chrome.driver",
-//					System.getProperty("user.dir") + "/src/main/resources/chromedriver");
-//			driver = new ChromeDriver();
-//		} else if (browserName.equals("firefox")) {
-//			System.setProperty("webdriver.gecko.driver",
-//					System.getProperty("user.dir") + "/src/main/resources/resources/geckodriver");
-//			driver = new FirefoxDriver();
-//		} else if (browserName.equals("ie")) {
-//			System.setProperty("webdriver.ie.driver",
-//					System.getProperty("user.dir") + "/src/main/resources/IEDriverServer");
-//			driver = new InternetExplorerDriver();
-//		} else {
-//			System.out.println(browserName + " is not a valid browser");
-//		}
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "/src/main/resources/chromedriver");
+			driver = new ChromeDriver();
+		} else if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					System.getProperty("user.dir") + "/src/main/resources/resources/geckodriver");
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("ie")) {
+			System.setProperty("webdriver.ie.driver",
+					System.getProperty("user.dir") + "/src/main/resources/IEDriverServer");
+			driver = new InternetExplorerDriver();
+		} else {
+			System.out.println(browserName + " is not a valid browser");
+		}
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
